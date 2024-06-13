f6de1be294948ba29188ff7f27434b60980101a5
Elvis M <u201112959@upc.edu.pe>
2024-06-13 09:14:59 -0500

    se completo la pagina new-movies
    se creo el componente PaginationMovies para navegar en todas las paginas que tenga almacenado informacion de peliculas de la base de datos

--- src/components/MovieCatalog/MovieCatalog.js
+++ src/components/MovieCatalog/MovieCatalog.js
@@ -1,7 +1,33 @@
+import { Card, Col, Row } from "antd";
+import { EyeOutlined } from "@ant-design/icons";
 import "./MovieCatalog.scss";
+import { Link } from "react-router-dom";
 
-const MovieCatalog = () => {
-  return "Movie Catalog...";
+const MovieCatalog = ({ movies: { results } }) => {
+  return results.map((movie) => (
+    <Col key={movie.id} span={6} className="movie-catalog">
+      <MovieCard movie={movie} />
+    </Col>
+  ));
 };
+
+function MovieCard({ movie }) {
+  const { id, title, poster_path } = movie;
+  const { Meta } = Card;
+  const posterPath = `https://image.tmdb.org/t/p/original${poster_path}`;
+
+  return (
+    <Link to={`/movie/${id}`}>
+      <Card
+        hoverable
+        style={{ width: 240 }}
+        cover={<img alt={title} src={posterPath} />}
+        actions={[<EyeOutlined />]}
+      >
+        <Meta title={title}></Meta>
+      </Card>
+    </Link>
+  );
+}
 
 export default MovieCatalog;
